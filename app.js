2b76c4edd29fba3da0acbe1badd4e08e1ee474e6
Seth Johnson <SethJohnsonCoding@gmail.com>
2023-04-26 11:28:13 -0700

    added previous repository progress without CSS or committs

--- app.js
+++ app.js
@@ -1,20 +1,86 @@
 /*----- constants -----*/
-const gameContainer = document.getElementById("gameContainer");
-const gameScore = document.getElementById("gameScore");
-const gameTime = document.getElementById("gameTime");
-const gameTarget = document.getElementById("gameTarget");
-const gameButton = document.getElementById("gameButton");
-
+const letterTarget = document.getElementById("letterTarget");
+const currentScore = document.getElementById("currentScore");
+const timeDisplay = document.getElementById("currentTime");
+const gameMessage = document.getElementById("gameMessage");
+const startButton = document.getElementById("startButton");
 
 /*----- state variables -----*/
 let score = 0;
 let timeRemaining = 60;
-let gameStart = false;
-let timeInterval; 
+let gameStarted = false;
+let timerInterval;
 
-/*-----event listeners ------*/
-gameButton.addEventListener("click", startGame);
+/*----- event listeners -----*/
 
+// Add event listener to start button
+startButton.addEventListener("click", function() {
+  if (!gameStarted) {
+    gameStarted = true;
+    score = 0;
+    timeRemaining = 60;
+    updateScore(false);
+    updateLetterTarget();
+    startButton.disabled = true;
+    gameMessage.textContent = "";
+    timerInterval = setInterval(updateTimer, 1000);
+  }
+});
+
+// Add event listener to document for keydown events
+document.addEventListener("keydown", function(event) {
+  if (gameStarted) {
+    const keyPressed = event.key.toLowerCase();
+    const targetLetter = letterTarget.textContent.toLowerCase();
+    if (keyPressed === targetLetter) {
+      updateLetterTarget();
+      updateScore(true);
+    } else {
+      updateScore(false);
+    }
+  }
+});
+
+// Define function to end the game
+function endGame(message) {
+  gameStarted = false;
+  startButton.disabled = false;
+  letterTarget.textContent = "";
+  clearInterval(timerInterval);
+  gameMessage.textContent = message;
+}
 
 /*----- functions -----*/
 
+// Define function to generate random lowercase letter
+function generateRandomLetter() {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const randomIndex = Math.floor(Math.random() * alphabet.length);
+  return alphabet[randomIndex];
+}
+
+// Define function to update the letter target
+function updateLetterTarget() {
+  const newLetter = generateRandomLetter();
+  letterTarget.textContent = newLetter;
+}
+
+// Define function to update the score
+function updateScore(isCorrect) {
+  if (isCorrect) {
+    score++;
+  } else {
+    score--;
+  }
+  currentScore.textContent = score;
+}
+
+// Define function to update the timer
+function updateTimer() {
+  timeRemaining--;
+  timeDisplay.textContent = timeRemaining;
+  if (timeRemaining === 0) {
+    alert("Stop Typing!");
+    endGame();
+  }
+}
