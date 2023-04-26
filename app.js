eed06f4c099cee2539bb512068c1809327bc2b69
Seth Johnson <SethJohnsonCoding@gmail.com>
2023-04-26 10:57:22 -0700

    made minor tweaks to constants in JS file

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 /*----- constants -----*/
 const gameContainer = document.getElementById("gameContainer");
-const score = document.getElementById("gameScore");
-const time = document.getElementById("gameTime");
-const target = document.getElementById("gameTarget");
+const gameScore = document.getElementById("gameScore");
+const gameTime = document.getElementById("gameTime");
+const gameTarget = document.getElementById("gameTarget");
 const gameButton = document.getElementById("gameButton");
 
 
 /*----- state variables -----*/
+
+
 
 
 /*----- functions -----*/
