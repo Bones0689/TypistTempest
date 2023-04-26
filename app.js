38cdb69609f16aa1974a5bd0a92d7ca010d09bdf
Seth Johnson <SethJohnsonCoding@gmail.com>
2023-04-26 10:54:49 -0700

    added constants

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 /*----- constants -----*/
+const gameContainer = document.getElementById("gameContainer");
+const score = document.getElementById("gameScore");
+const time = document.getElementById("gameTime");
+const target = document.getElementById("gameTarget");
+const gameButton = document.getElementById("gameButton");
+
 
 /*----- state variables -----*/
+
 
 /*----- functions -----*/
